400375d9a10cdad41c29f1e6bab9b1a013dba00e
Daniel Halverson <daniel.halverson@hotmail.com>
2020-05-27 10:27:12 -0600

    Update admin patrons test

--- lib/museum.rb
+++ lib/museum.rb
@@ -19,4 +19,5 @@ class Museum
     end
   end
 
+
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -87,8 +87,7 @@ class MuseumTest < Minitest::Test
       dmns.admit(patron_2)
       dmns.admit(patron_3)
 
-      assert_equal [patron_1, patron_2, patron_3], dmns.admit
+      assert_equal [patron_1, patron_2, patron_3], dmns.patrons
     end
 
-    end
 end
